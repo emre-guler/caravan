275ee2ec7b794aaa1244708e159c0b7bd2a04ecc
emre-guler <emre_guler01@hotmail.com>
2022-01-04 22:58:14 +0300

    Bugfix.

--- Models/Models/Login.cs
+++ Models/Models/Login.cs
@@ -6,12 +6,10 @@ namespace Caravan.Models
     {
         [RequiredArea]
         [CharacterLimit(Min = 6)]
-        [RegexControl(RegexPattern = "")]
         [ErrorMessage(Title = "MailAddressError", Message = "Bu alan boş geçilemez ve en az 6 karakterden oluşmalıdır.")]
         public string MailAddress { get; set; }
         [RequiredArea]
         [CharacterLimit(Min = 8)]
-        [RegexControl(RegexPattern = "")]
         [ErrorMessage(Title = "PasswordError", Message = "Bu alan boş geçilemez ve en az 8 karakterden oluşmalıdır.")]
         public string Password { get; set; }
     }
