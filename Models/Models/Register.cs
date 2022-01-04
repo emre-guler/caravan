e4dab8d17a127587754feda202b6798afc5f9d4f
emre-guler <emre_guler01@hotmail.com>
2022-01-04 22:15:23 +0300

    Regexler düzeltildi.

--- Models/Models/Register.cs
+++ Models/Models/Register.cs
@@ -13,22 +13,22 @@ namespace Caravan.Models
 
         [RequiredArea]
         [CharacterLimit(Min = 8)]
-        [RegexControl(RegexPattern = "")]
+        [RegexControl(RegexPattern = @"[a-zA-Z0-9]{8}")]
         [ErrorMessage(Title = "PasswordError", Message = "Bu alan boş geçilemez ve en az 8 karakterden oluşmalıdır.")]
         public string Password { get; set; }
 
 
         [RequiredArea]
         [CharacterLimit(Min = 6)]
-        [RegexControl(RegexPattern = "")]
+        [RegexControl(RegexPattern = @"/^\S+@\S+\.\S+$/")]
         [ErrorMessage(Title = "MailAddressError", Message = "Bu alan boş geçilemez ve en az 6 karakterden oluşmalıdır.")]
         public string MailAddress { get; set; }
 
 
         [RequiredArea]
         [CharacterLimit(Min = 9, Max = 12)]
-        [RegexControl(RegexPattern = "")]
-        [ErrorMessage(Title = "FullNameError", Message = "Bu alan boş geçilemez ve 9 -12 karakter arasında olmalıdır.")]
+        [RegexControl(RegexPattern = @"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$")]
+        [ErrorMessage(Title = "PhoneNumberError", Message = "Bu alan boş geçilemez ve 9 -12 karakter arasında olmalıdır.")]
         public string PhoneNumber { get; set; }
 
         
