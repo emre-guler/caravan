9c269bbf3cad025e88b417ab02b4e5f227c2fae2
emre-guler <emre_guler01@hotmail.com>
2022-01-10 01:54:21 +0300

    getSuppliersAddresses progress.

--- Service/Concrete/RecurringJobService.cs
+++ Service/Concrete/RecurringJobService.cs
@@ -7,19 +7,29 @@ namespace Caravan.Service
     public class RecurringJobService : IRecurringJobService
     {
         private readonly IRedisService _redisService;
+        private readonly ICustomerService _customerService;
         public RecurringJobService(
-            IRedisService redisService
+            IRedisService redisService,
+            ICustomerService customerService
         )
         {
             _redisService = redisService;
+            _customerService = customerService;
         }
         public async Task GetSuppliersAddresses()
-        {
+        {   
+            await _redisService.Add($"logs-info-{DateTime.UtcNow}-GetSuppliersAddresses", $"-GetSuppliersAddresses started.");
             try {
+                var userData = await _customerService.GetAllCustomers();
+                foreach (var user in userData)
+                {
+                    await _redisService.Add($"logs-info-{DateTime.UtcNow}-GetSuppliersAddressesForUser", $"-GetSuppliersAddresses started for. UserId: {user.Id}");
+                    
+                }
 
             }
             catch (Exception e) {
-                
+                await _redisService.Add($"logs-fail-{DateTime.UtcNow}-GetSuppliersAddresses", $"GetSuppliersAddresses failed. {e.ToString()}");
             }
         }
     }
